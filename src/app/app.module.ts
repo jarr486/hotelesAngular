8804fa2a961db80bbefe91c47cceba07f3165bc3
jarr486 <jarr486@gmail.com>
2018-09-15 16:59:04 +0000

    Nuevos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  //Declarar variables
   btnIniciarSesion:string;
+  btnHoteles:string;
+  
+  btnCategorias:Array<string>;
   
   constructor() {
     
+    //Inicializo
     this.btnIniciarSesion="Inciar Sesion";
+    this.btnHoteles ="Tipos Hoteles";
+    
+    this.btnCategorias = [
+         'Una Estrellas',
+         'Dos Estrellas',
+         'Tres Estrellas',
+         'Cuatro Estrellas',
+         'Cinco Estrellas'
+    ];
     
   }
 
